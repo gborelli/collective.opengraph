37bb4edb7d3c03e5d64ecad2f6231a02149ba52d
RedTurtle <redturtle@redturtle.local>
2012-10-13 17:51:31 +0200

    remove

--- setup.py
+++ setup.py
@@ -37,6 +37,4 @@ setup(name='collective.opengraph',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
